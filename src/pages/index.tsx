64698ae677ef5ce3b502a63ab73bf253296ff761
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-12-31 14:15:05 +0100

    use relative path for stream

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,7 +79,7 @@ function Index() {
         }}
       />
       <Main>
-        <LiveVideoPlayer width={1280} height={720} src="https://frikanalen.no/stream/index.m3u8" />
+        <LiveVideoPlayer width={1280} height={720} src="/stream/program.m3u8" />
         {renderSchedule()}
       </Main>
       <Sidebar>
