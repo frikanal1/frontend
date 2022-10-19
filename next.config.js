703df307c8ce34908e3efb4468b9b592f4b44809
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-10-19 15:01:16 +0200

    Add some security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require("next-remove-imports")()
 
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+]
+
 module.exports = removeImports({
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ]
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false, os: false, crypto: "crypto" }
 
