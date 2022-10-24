245e46f504d2df0c0c1f6cd162a4cd49367428f7
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-10-24 09:42:08 +0200

    Minor cleanups

--- src/modules/core/constants.ts
+++ src/modules/core/constants.ts
@@ -4,8 +4,7 @@ export const GENERAL_BREAKPOINT = "700px"
 
 export const MOBILE_MENU_THRESHOLD = 600
 
-export const IS_SERVER = typeof window === "undefined"
-export const ARTIFICIAL_DELAY = IS_SERVER ? 0 : 200
+export const IS_SERVER_SIDE = typeof window === "undefined"
 
 export const CANONICAL_HOST = "https://frikanalen.no"
 export const WEBSITE_NAME = "Frikanalen"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import React from "react"
-import type { AppProps } from "next/app"
 import { Header } from "src/modules/core/components/Header"
 import { global } from "src/modules/styling/global"
 import { Main } from "src/modules/core/components/Main"
 import { Footer } from "src/modules/core/components/Footer"
 import { ThemeContext } from "src/modules/styling/components/ThemeContext"
 import { GlobalStyles } from "@mui/material"
-import { client } from "../modules/apollo/client"
-import { ApolloProvider } from "@apollo/client"
+import { ApolloClient, ApolloProvider } from "@apollo/client"
 import { UserProvider } from "src/refactor/UserContext"
+import { AppProps } from "next/app"
+import { client } from "../modules/apollo/client"
 
-const CustomApp = ({ Component, pageProps }: AppProps) => (
-  <ApolloProvider client={client}>
+type FKAppProps = AppProps & {
+  apolloClient?: ApolloClient<object>
+}
+
+const CustomApp = ({ Component, pageProps, apolloClient = client }: FKAppProps) => (
+  <ApolloProvider client={apolloClient}>
     <UserProvider>
       <ThemeContext>
         <GlobalStyles styles={global} />
