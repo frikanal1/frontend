3b5a3d4ad1c7f134e5b0f7734685b9f1f2ae5cb8
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-11-08 02:18:22 +0100

    One more style iteration

--- src/modules/bulletins/FrontpageBulletins.tsx
+++ src/modules/bulletins/FrontpageBulletins.tsx
@@ -13,7 +13,7 @@ export const FrontpageBulletins = () => {
     <div className="space-y-4">
       {bulletins?.map(({ id, title, createdAt, text }) => (
         <div key={id} className={"shadow-md"}>
-          <div className={"bg-gradient-to-b from-red-800 via-red-900 to-red-900 text-red-100 p-4"}>
+          <div className={"bg-gradient-to-b from-red-900/80 to-red-900/95 text-red-100 p-4"}>
             <h4 className={"lg:text-3xl font-bold tracking-wide"}>{title}</h4>
             <div className={"text-red-200 text-lg"}>{format(new Date(createdAt), "d. MMM Y", { locale: nb })}</div>
           </div>
