b8e960e9a3637d46965374390246c10a71a56c75
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-11-15 01:49:00 +0100

    Mobile styling of search

--- src/refactor/searchFunction.tsx
+++ src/refactor/searchFunction.tsx
@@ -22,12 +22,12 @@ const SearchResults = ({ results, show }: { results?: VideoSearchResultFragment[
   )
 }
 const SearchResult = ({ result }: { result: VideoSearchResultFragment }) => (
-  <div className={" pl-2 pr-5 pb-[5px] text-black/80 hover:text-green-900 hover:bg-green-200"}>
+  <div className={"pl-2 pr-5 pb-[5px] text-black/80 hover:text-green-900 hover:bg-green-200"}>
     <Link href={`/video/${result.id}`}>
-      <div className={"flex gap-2 items-center text-lg"}>
-        <img className={"w-16 aspect-video block"} alt={""} src={result.images.thumbSmall} />
-        <div className={"text-black/60"}>{result.organization.name}</div>
-        <div className={""}>{result.title}</div>
+      <div className={"flex gap-2 lg:items-center text-sm lg:text-lg"}>
+        <img className={"lg:basis-16 aspect-video block hidden lg:hidden-0"} alt={""} src={result.images.thumbSmall} />
+        <div className={"basis-1/7 lg:block"}>{result.organization.name}</div>
+        <div className={"grow"}>{result.title}</div>
       </div>
     </Link>
   </div>
@@ -52,7 +52,7 @@ export const SearchFunction = ({ className }: { className?: string }) => {
           "z-50 bg-gradient-to-b from-green-600 to-green-800 p-2 flex font-bold text-2xl text-white/95 items-center"
         }
       >
-        <div className={"px-5 ml-5 py-3"}>Søk</div>
+        <div className={"px-5 ml-5 py-3 hidden lg:hidden-0 "}>Søk</div>
         <div className={"bg-green-100 rounded-lg m-1 grow text-black " + (showResults ? "rounded-b-none" : "")}>
           <div className={"relative"}>
             <form role={"search"} onSubmit={(e) => e.preventDefault()}>
