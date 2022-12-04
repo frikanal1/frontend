682543147e3d61fcd1f4770e4fdf3ef41c553975
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-12-04 21:29:00 +0100

    Delete disused file

--- src/refactor/ModuleHeading.tsx
+++
@@ -1,8 +0,0 @@
-// Headline of major page components
-export const ModuleHeading = ({ children, className }: { children: string; className?: string }) => (
-  <h3
-    className={"text-2xl font-wide lg:text-4xl text-gray-800 pb-5 font-sans font-bold tracking-wide " + className ?? ""}
-  >
-    {children}
-  </h3>
-)
