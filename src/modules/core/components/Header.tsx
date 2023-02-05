1610d12d9831c9e58a337a70a2260da48e3049a8
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-02-05 13:06:02 +0100

    Tweak header logo margins for mobileˆ

--- src/modules/core/components/Header.tsx
+++ src/modules/core/components/Header.tsx
@@ -7,7 +7,7 @@ import { MobileNavLinks } from "./MobileNavLinks"
 // margins to "crop" logo to text: lg:ml-[-113px] lg:mb-[-55px]
 export const Header = ({ className }: { className?: string }) => (
   <header className={className}>
-    <div className={"flex items-center justify-between w-full pb-3 md:py-2 lg:py-4"}>
+    <div className={"flex items-center justify-between w-full pl-1 pb-3 md:py-2 lg:py-4"}>
       <Link aria-label="Forsiden" href={"/"} passHref>
         <Logo className={"w-40 md:w-72 xl:w-[500px] cursor-pointer"} />
       </Link>
