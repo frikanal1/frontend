3c52375d50ffd7bd76c3b4fcc2df6d92cc3e50b6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-02-05 22:05:49 +0100

    Front page design tweaks

--- src/modules/schedule/components/FrontpageScheduleView.tsx
+++ src/modules/schedule/components/FrontpageScheduleView.tsx
@@ -8,11 +8,10 @@ export const FrontpageScheduleView = () => {
   if (!schedule) return null
 
   return (
-    <div className="p-4 xl:p-8">
+    <div className="p-4 xl:px-8 xl:py-6">
       <CurrentProgramme entry={current(0)} />
-      <div className={"md:text-xl text-green-100/70 font-bold font-condensed xl:pt-2"}>senere</div>
       <UpcomingProgramme className="text-green-200 font-light" entry={current(1)} />
-      <UpcomingProgramme className="text-green-300 font-light" entry={current(2)} />
+      <UpcomingProgramme className="text-green-200 font-light" entry={current(2)} />
     </div>
   )
 }
--- src/modules/schedule/components/UpcomingProgramme.tsx
+++ src/modules/schedule/components/UpcomingProgramme.tsx
@@ -11,17 +11,17 @@ export interface UpcomingProgrammeProps {
 export function UpcomingProgramme({ className, entry }: UpcomingProgrammeProps) {
   return (
     <div className={cx("flex w-full flex-col xl:flex-row pb-2", className)}>
+      <span className={"font-condensed w-24  pr-2 shrink-0 font-bold"}>
+        {entry && humanizeScheduleItemDate(new Date(entry?.start))}
+      </span>
       <div className={"grow flex flex-col"}>
         <Link href={entry?.video.url || ""} passHref className={"font-bold truncate w-full"}>
           {entry?.video.title}
         </Link>
-        <Link href={`/organization/${entry?.video.organization.id}`} passHref className={""}>
+        <Link href={`/organization/${entry?.video.organization.id}`} passHref className={"opacity-90"}>
           {entry?.video.organization.name}
         </Link>
       </div>
-      <span className={"font-condensed text-green-400 shrink-0 font-bold"}>
-        {entry && humanizeScheduleItemDate(new Date(entry?.start))}
-      </span>
     </div>
   )
 }
