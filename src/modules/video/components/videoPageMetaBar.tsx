e8df85de9e3b546ad816585aeedcea2a8a1646b6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-02-05 17:21:56 +0100

    VOD page design tweak

--- src/modules/video/components/videoPageMetaBar.tsx
+++ src/modules/video/components/videoPageMetaBar.tsx
@@ -15,24 +15,18 @@ export const VideoPageMetaBar = ({
   return (
     <div className={className}>
       <div className={"p-4 lg:p-6 bg-gradient-to-tr from-gray-800/80 to-gray-500/80 mix-blend-luminosity "}>
-        <h2 className={"lg:text-3xl lg:pb-1 text-white font-bold mix-blend-luminosity opacity-80"}>{title}</h2>
-        <div className={"py-1 lg:pt-2"}>
-          <span className={"text-white opacity-60 lg:text-xl font-condensed"}>
-            publisert{" "}
-            <span className={"font-semibold"}>{format(new Date(createdAt), "d. MMM yyyy", { locale: nb })}</span> av
-          </span>
-          <h3 className={"inline lg:block lg:text-4xl text-gray-300 hover:text-white opacity-90 font-semibold py-2"}>
-            <Link href={`/organization/${organization.id}`} passHref>
-              {" "}
-              {organization.name}
-            </Link>
-          </h3>
-          <div>
-            <div className={"text-white opacity-60 lg:text-xl bold font-condensed hidden lg:block"}>beskrivelse</div>
-            <div className={"whitespace-pre-wrap break-words lg:py-2 prose-invert prose opacity-90 lg:prose-xl"}>
-              <ReactMarkdown>{description}</ReactMarkdown>
-            </div>
-          </div>
+        <h2 className={"lg:text-3xl text-white font-bold mix-blend-luminosity opacity-80"}>{title}</h2>
+        <h3 className={"inline lg:block lg:text-2xl text-gray-300 hover:text-white opacity-90 font-semibold"}>
+          <Link href={`/organization/${organization.id}`} passHref>
+            {organization.name}
+          </Link>
+        </h3>
+        <div className={"text-white opacity-60 lg:text-xl"}>
+          publisert{" "}
+          <span className={"font-semibold"}>{format(new Date(createdAt), "d. MMM yyyy", { locale: nb })}</span>
+        </div>
+        <div className={"whitespace-pre-wrap break-words lg:pt-4 !leading-7 prose-invert prose opacity-80 lg:prose-lg"}>
+          <ReactMarkdown>{description}</ReactMarkdown>
         </div>
       </div>
     </div>
